997df1d724c784c0587c02d5e82d668bb3b808b3
Kuei-Jung Hu <kueijunghu@gmail.com>
2022-04-07 19:56:10 +0900

    Gain mapping

--- Source/KAPGain.cpp
+++ Source/KAPGain.cpp
@@ -25,7 +25,10 @@ void KAPGain::process(float* inAudio,
                       float* outAudio,
                       int inNumSamplesToRender)
 {
+    float gainMapped = juce::jmap(inGain, 0.0f, 1.0f, -24.0f, 24.0f);
+    gainMapped = juce::Decibels::decibelsToGain(gainMapped, -24.0f);
+    
     for(int i = 0; i < inNumSamplesToRender; i++) {
-        outAudio[i] = inAudio[i] * inGain;
+        outAudio[i] = inAudio[i] * gainMapped;
     }
 }
