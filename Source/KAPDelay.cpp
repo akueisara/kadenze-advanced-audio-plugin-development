f58a7a9ae5b19937426f6525ab92c162429e84c7
Kuei-Jung Hu <kueijunghu@gmail.com>
2022-04-07 20:35:50 +0900

    Implement the Delay module

--- Source/KAPDelay.cpp
+++ Source/KAPDelay.cpp
@@ -9,3 +9,39 @@
 */
 
 #include "KAPDelay.h"
+
+#include <JuceHeader.h>
+
+KAPDelay::KAPDelay()
+:   mSampleRate(-1),
+    mFeedbackSample(0.0f),
+    mDelayIndex(0)
+{
+    
+}
+KAPDelay::~KAPDelay()
+{
+    
+}
+
+void KAPDelay::setSampleRate(double inSampleRate)
+{
+    mSampleRate = inSampleRate;
+}
+
+void KAPDelay::reset()
+{
+    juce::zeromem(mBuffer, sizeof(double) * 2048);
+}
+
+void KAPDelay::process(float* inAudio,
+             float inTime,
+             float inFeedback,
+             float inWetDry,
+             float* outAudio,
+             int inNumSamplesToRender)
+{
+    const float wet = inWetDry;
+    const float dry = 1.0f - wet;
+    const float feedbackMapped = juce::jmap(inFeedback, 0.0f, 1.0f, 0.0f, 0.95f);
+}
--- Source/KAPDelay.h
+++ Source/KAPDelay.h
@@ -9,3 +9,30 @@
 */
 
 #pragma once
+
+class KAPDelay
+{
+public:
+    
+    KAPDelay();
+    ~KAPDelay();
+    
+    void setSampleRate(double inSampleRate);
+    
+    void reset();
+    
+    void process(float* inAudio,
+                 float inTime,
+                 float inFeedback,
+                 float inWetDry,
+                 float* outAudio,
+                 int inNumSamplesToRender);
+    
+private:
+    
+    double mSampleRate;
+    double mBuffer[2048];
+    double mFeedbackSample;
+    
+    int mDelayIndex;
+};
